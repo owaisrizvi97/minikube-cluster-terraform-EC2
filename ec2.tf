214023f83e8bc19e7eb2c41d9d02cf8c57aef029
Owais Rizvi <68285890+owaisrizvi97@users.noreply.github.com>
2023-02-21 13:23:29 +0500

    Terraform project create minikube cluster on AWS

---
+++ ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "my_ec2_instance" {
+	ami = "ami-0caf778a172362f1c"
+	instance_type = "t2.medium"
+	availability_zone = "ap-south-1a"
+	vpc_security_group_ids = [aws_security_group.allow_tls.id]
+	key_name = "MyNewKey"
+	tags = {
+		name = "minikube_instance"
+	}
+provisioner "remote-exec" {
+	inline = [
+     		"sudo apt update -y",
+      		"sudo apt install docker.io -y",
+      		"sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      		"sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+      		"sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+      		"curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+      		"sudo chmod +x /home/ubuntu/kubectl",
+      		"sudo cp kubectl /usr/local/bin/kubectl",
+      		"sudo usermod -aG docker ubuntu"
+	]
+	connection {
+		type = "ssh"
+		host = self.public_ip
+		user = "ubuntu"
+		private_key = file("./MyNewKey.pem")
+
+	}
+   }
+
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_access_key = "AKIAQLN7W56UGT7THR63"
+aws_secret_key = "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"
+
